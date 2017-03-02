716542e778670eae6cabbfc773094504f06de31f
Yevhen Vakulchyk <Yevhen Vakulchyk>
2017-03-02 13:01:53 +0200

    npm .env added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
+var dotenv = require('dotenv');
+dotenv.load();
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
 var connector = new builder.ChatConnector();
 var bot = new builder.UniversalBot(connector);
 
