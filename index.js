72ea3ea2ce3c9e7900a2d2cd6bb295a5bb09053f
Yevhen Vakulchyk <Yevhen Vakulchyk>
2017-03-03 21:14:46 +0200

    env 1.0.1

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// var connector = new builder.ChatConnector();
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
